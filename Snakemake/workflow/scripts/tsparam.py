d3d24baa7667f0faa3d3fe518bc1f6e198c4458c
Hannes Becher <hannesbecher@gmx.de>
2023-11-27 10:21:42 +0000

    ts params adjusted for bee test data

--- Snakemake/workflow/scripts/tsparam.py
+++ Snakemake/workflow/scripts/tsparam.py
@@ -1,7 +1,9 @@
 # parameters for tsinfer --- change accordingly
 threads=20
-lwertime=0.4
-uprtime=0.6
+#lwertime=0.4 # cattle
+#uprtime=0.6 # cattle
+lwertime=0.0 # bee test data
+uprtime=0.1 # bee test data
 lenmultiply=2
 recombratio=1.1e-8
 mismtachratio=1
